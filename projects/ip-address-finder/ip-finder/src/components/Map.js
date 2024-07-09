e3bdfadfb25b507cc582f6c3ed7abd2168c9fa47
Vasanth Gopal <vasanth1627@gmail.com>
2024-07-10 00:08:14 +0530

    updated readme on jokes

--- projects/ip-address-finder/ip-finder/src/components/Map.js
+++ projects/ip-address-finder/ip-finder/src/components/Map.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import ReactMapGL, { Marker} from 'react-map-gl';
 import { RiUserLocationFill} from 'react-icons/ri';
 
-const API_KEY ='pk.eyJ1IjoidmFzYW50aGd4IiwiYSI6ImNseWVtNmt0OTAzNTgybXNlY2g1bmt4Z3EifQ.d4BHSv2CHK9UKyWGkSrVEg';
+const API_KEY ='pk.eyJ1IjoidmFzYW50aGd4IiwiYSI6ImNseWVvM2pkajA0eHQya3NkaHpiYWYxYmoifQ.YaN6FSJSg0k_iFC38-vfvw';
 const Map = ({lat,lon})=>{
     const [viewport, setViewport] = useState({
         latitude:lat,
@@ -30,8 +30,8 @@ const Map = ({lat,lon})=>{
             onViewportChange={(viewport)=>{
                 setViewport(viewport)
             }}
-            // mapStyle= "mapbox://styles/mapbox/streets-v11"
-            mapStyle= "mapbox://styles/mapbox/empty"
+            mapStyle= "mapbox://styles/mapbox/streets-v11"
+            // mapStyle= "mapbox://styles/mapbox/empty"
             >
             <Marker latitude={lat} longitude={lon}>
                 <div className="mark">
