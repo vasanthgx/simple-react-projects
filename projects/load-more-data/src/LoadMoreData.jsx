be17528c10071509170fbba6f8506e593a92534a
Vasanth Gopal <vasanth1627@gmail.com>
2024-07-27 16:07:30 +0530

    successfully fetched data from API and displayed

--- projects/load-more-data/src/LoadMoreData.jsx
+++ projects/load-more-data/src/LoadMoreData.jsx
@@ -1,10 +1,65 @@
+import { useEffect, useState } from "react"
 
 
 export default function LoadMoreData(){
+    const [loading, setLoading] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [count, setCount] = useState(0);
+
+    async function fetchImages(){
+
+        try {
+            setLoading(true);
+            const apiResponse = await fetch(`https://dummyjson.com/products?limit=20&skip=${count === 0 ?0: count * 20}`);
+            const data = await apiResponse.json();
+            if(data && data.products && data.products.length){
+                setProducts((prevData)=> [...prevData, ...data.products]);
+            }
+
+            setLoading(false);
+
+            console.log(data)
+            
+        } catch (error) {
+            console.log(error.message);
+            setLoading(false);
+        }
+
+    }
+
+
+    useEffect(()=>{
+
+            fetchImages()
+    },[count]);
+    
+    if(loading) {
+        return <div>Loading in process... please wait</div>
+
+    }
 
     return (
-        <div>
-        Load More Data
+        <div className="initial-container">
+         <div className="image-container">
+            {
+                products && products.length ? 
+                products.map(item=>(
+                    <div className="product">
+                        <img
+                            src={item.thumbnail}
+                            alt={item.title}
+
+                        />
+                        <p>{item.title}</p>
+                    </div>
+                ))
+                : null
+
+            }
+         </div>
+         <div className="button-container">
+            <button>Load more data</button>
+         </div>
         
         </div>
     )
