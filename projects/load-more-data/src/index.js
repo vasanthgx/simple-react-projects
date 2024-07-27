fa589beb68b6f1f5ed041cecbd3921950a6f2dc4
Vasanth Gopal <vasanth1627@gmail.com>
2024-07-27 17:04:14 +0530

    removed the strict mode, since  the app was rendering the images twice.

--- projects/load-more-data/src/index.js
+++ projects/load-more-data/src/index.js
@@ -7,9 +7,9 @@ import LoadMoreData from './LoadMoreData';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
     <LoadMoreData/>
-  </React.StrictMode>
+  
 );
 
 
