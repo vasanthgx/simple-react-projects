da27548b3204b8c94163997214a423aae17df843
Vasanth Gopal <vasanth1627@gmail.com>
2024-08-23 19:38:28 +0530

    initial commit

---
+++ projects/light-dark-mode/src/LightDarkMode.jsx
@@ -0,0 +1,12 @@
+
+
+export default function LightDarkMode() {
+  return (
+    <div className="light-dark-mode" >
+      <div className="container" >
+            <p>Hello World !!!</p>
+            <button>Change Mode</button>
+      </div>
+    </div>
+  )
+}
