5884f68f9c9c48b0170ebff2e5391a6365fcb070
Vasanth Gopal <vasanth1627@gmail.com>
2024-08-26 19:51:12 +0530

    logging data in the console

--- projects/scroll-indicator/src/ScrollIndicator.jsx
+++ projects/scroll-indicator/src/ScrollIndicator.jsx
@@ -1,6 +1,31 @@
 
+import { useEffect, useState } from 'react';
+import  './scroll.css' ;
+function ScrollIndicator({url}) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);  
+  const [errorMessage, setErrorMessage] = useState('');
 
-function ScrollIndicator() {
+  async function fetchData(getUrl){
+    try {
+       setLoading(true);
+       const response = await fetch(getUrl);
+       const data = await response.json();
+       setData(data);
+       setLoading(false); 
+
+       console.log(data);
+      
+    } catch (error) {
+      console.log(error);
+      setErrorMessage(error.message);
+
+    }
+  }
+
+  useEffect(() => {
+    fetchData(url);
+  }, [url]);
   return (
     <div >
      scroll
--- projects/scroll-indicator/src/index.js
+++ projects/scroll-indicator/src/index.js
@@ -5,7 +5,7 @@ import ScrollIndicator from './ScrollIndicator';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ScrollIndicator/>
+    <ScrollIndicator url={'https://dummyjson.com/products?limit=100'}/>
   </React.StrictMode>
 );
 
