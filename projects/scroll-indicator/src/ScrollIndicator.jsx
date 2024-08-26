b658a380eea2e3f206e0d0651851604cc2f35755
Vasanth Gopal <vasanth1627@gmail.com>
2024-08-26 20:28:39 +0530

    logging the percentage of scroll

--- projects/scroll-indicator/src/ScrollIndicator.jsx
+++ projects/scroll-indicator/src/ScrollIndicator.jsx
@@ -5,6 +5,7 @@ function ScrollIndicator({url}) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);  
   const [errorMessage, setErrorMessage] = useState('');
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
   async function fetchData(getUrl){
     try {
@@ -29,11 +30,37 @@ function ScrollIndicator({url}) {
     fetchData(url);
   }, [url]);
 
+  function handleScrollPercentage(){
+    console.log(
+      document.body.scrollTop,
+      document.documentElement.scrollTop,
+      document.documentElement.scrollHeight,
+      document.documentElement.clientHeight
+    )
+    const scroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+    setScrollPercentage( (scroll/height)*100 )
+
+  }
+
+  useEffect(()=>{
+    window.addEventListener('scroll', handleScrollPercentage);
+
+    window.removeEventListener('scroll', ()=>{})
+  },[])
+
   
-       console.log(data,loading);
+       console.log(data,scrollPercentage);
   return (
     <div >
       <h1>Scroll Indicator</h1>
+      <div className='scroll-progress-container'>
+        <div className="progress-bar">
+          
+        </div>
+
+      </div>
       <div className="data-container">
         {
            data && data.length ? 
